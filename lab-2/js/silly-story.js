b9dd0c29f10a9820b03f9720d4ee022f0410db04
dillonjenkins10 <105180992+dillonjenkins10@users.noreply.github.com>
2025-06-04 17:07:25 -0400

    Added variables and onclick event for the button

--- lab-2/js/silly-story.js
+++ lab-2/js/silly-story.js
@@ -6,15 +6,23 @@
 - story for the paragraph that outputs the final story
 */
 
+let customName = 'Bob';
+let randomize = document.getElementById('randomizer');
+let story = 'placeholder for story';
+
 /* STEP 3: Create the variable that contains the story string that will be modified - use var storyText to containt the following:
 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
 */
+var storyText = 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 
 /* STEP 4: Create three arrays, insertX, insertY, and insertZ, assigning them the following array values respectively:
 Donald Trump, Jackie Chan, Santa Claus
 Area 51, Death Valley, Aruba
 spontaneously combusted, rapidly sublimated, evaporated instantly
 */
+let insertX = ['Donald Trump', 'Jackie Chan', 'Santa Claus'];
+let insertY = ['Area 51', 'Death Valley', 'Aruba'];
+let insertZ = ['spontaneously combusted', 'rapidly sublimated', 'evaporated instantly'];
 
 // FUNCTIONS
 
@@ -26,7 +34,7 @@ function randomValueFromArray(array){
 /* STEP 6: Review the partially complete result() function below */
 function result() {
 	// STEP 7: Create a new variable called newStory and set it to the value of storyText - we don't want to overwrite the original story!
-
+	
 	/* STEP 8: Use the randomValueFromArray() function to generate a value for each of three new variables - xItem, yItem, and zItem
 	Call up the function and for each variable, pass it the array from which to grab a random string - for example if insertW was an array of strings, I would type:
 	var wItem = randomValueFromArray(insertW);
@@ -55,7 +63,9 @@ function result() {
 
 // EVENT LISTENERS
 /* STEP 5: Add a click event listener to the randomize variable so that when the button it represents is clicked, the result() function is run. */
-
+randomize.onclick = function() {
+	result();
+};
 
 // This lab based on the excellent assessment challenge at https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Silly_story_generator
 
