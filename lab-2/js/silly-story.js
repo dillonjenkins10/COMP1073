36b2fb5d8b7b3c3c84151c8b01b259b130f128f7
dillonjenkins10 <105180992+dillonjenkins10@users.noreply.github.com>
2025-06-04 17:23:01 -0400

    Added text replacement, working on unit conversion

--- lab-2/js/silly-story.js
+++ lab-2/js/silly-story.js
@@ -8,7 +8,7 @@
 
 let customName = 'Bob';
 let randomize = document.getElementById('randomizer');
-let story = 'placeholder for story';
+let story = document.createElement('p');
 
 /* STEP 3: Create the variable that contains the story string that will be modified - use var storyText to containt the following:
 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
@@ -34,28 +34,42 @@ function randomValueFromArray(array){
 /* STEP 6: Review the partially complete result() function below */
 function result() {
 	// STEP 7: Create a new variable called newStory and set it to the value of storyText - we don't want to overwrite the original story!
+	var newStory = storyText;
 	
 	/* STEP 8: Use the randomValueFromArray() function to generate a value for each of three new variables - xItem, yItem, and zItem
 	Call up the function and for each variable, pass it the array from which to grab a random string - for example if insertW was an array of strings, I would type:
-	var wItem = randomValueFromArray(insertW);
+	var wItem = randomValueFromArray(insertW); */
+	var xItem = randomValueFromArray(insertX);
+	var yItem = randomValueFromArray(insertY);
+	var zItem = randomValueFromArray(insertZ);
 
 	/* STEP 9: Replace the three placeholders in the newStory string — :insertx:, :inserty:, and :insertz: — with the strings stored in xItem, yItem, and zItem. Each time, be sure to update the variable newStory (with +=). You might need to do one of the above replacements twice! */
+	newStory = newStory.replace(':insertX:',xItem);
+	newStory = newStory.replace(':insertY:',yItem);
+	newStory = newStory.replace(':insertZ:',zItem);
+	newStory = newStory.replace(':insertX:',xItem);
+	console.log(newStory);
 
 	/* STEP 10: If the user has typed a name in the customName field, replace the name 'Bob' in the story with whatever they typed */
 	if(customName.value != '') {
-
+		newStory = newStory.replace('Bob', customName.value);
 	}
 	/* STEP 11: If the metric radio button has been checked, we need to convert the temperature and mass numbers in the story */
   if(document.getElementById("metric").checked) {
 		// STEP 11a: Create a variable called weight and convert the 300lbs to kgs (1lb = 0.453592kg)
+		var weight = 300 * 0.453592;
 		// STEP 11b: Replace the string 300 pounds with the updated weight in kg
+		newStory = newStory.replace('300 pounds', weight +' kilograms');
 
 		// STEP 12a: Create a variable called temp and convert °F to °C ... the formula for conversion is °C = (°F - 32) x 5/9
+		var temp = (94 - 32) * 5/9;
 
 		// STEP 12b: Replace the string '94 fahrenheit' with the updated temperature in °C
+		
 
   }
 	/* STEP 13: Make the textContent property of the story variable (which references the paragraph) equal to newStory */
+	story.textContent = newStory;
 
 	// The following line makes the paragraph visible
 	story.style.visibility = 'visible';
